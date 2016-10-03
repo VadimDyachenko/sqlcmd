b531a94af1ed15cd0c54b266303be1de20be2619
Vadim Dyachenko <vadim.dyachenko@gmail.com>
2016-10-03 22:18:56 +0300

    Добавил тесты всех пунктов меню.

--- src/test/java/sqlcmd/integration/IntegrationTest.java
+++ src/test/java/sqlcmd/integration/IntegrationTest.java
@@ -1,15 +1,12 @@
 package sqlcmd.integration;
 
 
-
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import sqlcmd.Controller;
 
 
 import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 
@@ -18,11 +15,11 @@ import static org.junit.Assert.assertEquals;
 
 public class IntegrationTest {
 
-    private  ConfigurableInputStream in;
-    private  ByteArrayOutputStream out;
+    private ConfigurableInputStream in;
+    private ByteArrayOutputStream out;
 
     @Before
-    public void setup(){
+    public void setup() {
         in = new ConfigurableInputStream();
         out = new ByteArrayOutputStream();
         System.setIn(in);
@@ -30,7 +27,7 @@ public class IntegrationTest {
     }
 
     @Test
-    public void testExit(){
+    public void testExit() {
         //given
         in.addLine("4");
         in.addLine("y");
@@ -52,12 +49,11 @@ public class IntegrationTest {
     }
 
     @Test
-    public void testTerminatedExit(){
+    public void testTerminatedExit() {
         //given
         in.addLine("exit");
         //when
         Controller.main(new String[0]);
-
         //then
         assertEquals("Welcome to SQLCmd!\n" +
                 "\n" +
@@ -66,7 +62,332 @@ public class IntegrationTest {
                 "2 - List all table names\n" +
                 "3 - Select table to work\n" +
                 "4 - Exit\n" +
+                //input - exit
+                "Terminated. Thank you for using SQLCmd. Good luck.\n", getData());
+    }
 
+    @Test
+    public void testListWithoutConnect() {
+        //given
+        in.addLine("2");
+        in.addLine("exit");
+        //when
+        Controller.main(new String[0]);
+        //then
+        assertEquals("Welcome to SQLCmd!\n" +
+                "\n" +
+                "Please choose an operation desired or type 'EXIT' for exiting\n" +
+                "1 - Connect to database\n" +
+                "2 - List all table names\n" +
+                "3 - Select table to work\n" +
+                "4 - Exit\n" +
+                // input - 2
+                "No one connection to database. Select \"Connect to database\" first.\n" +
+                "\n" +
+                "Please choose an operation desired or type 'EXIT' for exiting\n" +
+                "1 - Connect to database\n" +
+                "2 - List all table names\n" +
+                "3 - Select table to work\n" +
+                "4 - Exit\n" +
+                //input - exit
+                "Terminated. Thank you for using SQLCmd. Good luck.\n", getData());
+    }
+
+    @Test
+    public void testSelectTableWithoutConnect() {
+        //given
+        in.addLine("3");
+        in.addLine("exit");
+        //when
+        Controller.main(new String[0]);
+        //then
+        assertEquals("Welcome to SQLCmd!\n" +
+                "\n" +
+                "Please choose an operation desired or type 'EXIT' for exiting\n" +
+                "1 - Connect to database\n" +
+                "2 - List all table names\n" +
+                "3 - Select table to work\n" +
+                "4 - Exit\n" +
+                // input - 3
+                "No one connection to database. Select \"Connect to database\" first.\n" +
+                "\n" +
+                "Please choose an operation desired or type 'EXIT' for exiting\n" +
+                "1 - Connect to database\n" +
+                "2 - List all table names\n" +
+                "3 - Select table to work\n" +
+                "4 - Exit\n" +
+                //input - exit
+                "Terminated. Thank you for using SQLCmd. Good luck.\n", getData());
+    }
+
+    @Test
+    public void testUnsupportedChoise() {
+        //given
+        in.addLine("0");
+        in.addLine("exit");
+        //when
+        Controller.main(new String[0]);
+        //then
+        assertEquals("Welcome to SQLCmd!\n" +
+                "\n" +
+                "Please choose an operation desired or type 'EXIT' for exiting\n" +
+                "1 - Connect to database\n" +
+                "2 - List all table names\n" +
+                "3 - Select table to work\n" +
+                "4 - Exit\n" +
+                // input - 0
+                "\n" +
+                "Please choise correct number:\n" +
+                "1 - Connect to database\n" +
+                "2 - List all table names\n" +
+                "3 - Select table to work\n" +
+                "4 - Exit\n" +
+                //input - exit
+                "Terminated. Thank you for using SQLCmd. Good luck.\n", getData());
+    }
+
+    @Test
+    public void testConnect() {
+        //given
+        in.addLine("1");
+        in.addLine("sqlcmd");
+        in.addLine("javauser");
+        in.addLine("test");
+        in.addLine("exit");
+        //when
+        Controller.main(new String[0]);
+        //then
+        assertEquals("Welcome to SQLCmd!\n" +
+                "\n" +
+                "Please choose an operation desired or type 'EXIT' for exiting\n" +
+                "1 - Connect to database\n" +
+                "2 - List all table names\n" +
+                "3 - Select table to work\n" +
+                "4 - Exit\n" +
+                // input - 1
+                "Enter database name, login and password.\n" +
+                "Type 'exit' for exit program.\n" +
+                "\n" +
+                "Please, enter database name:\n" +
+                "Enter you login:\n" +
+                "Enter you password:\n" +
+                "Connection successful!\n" +
+                "\n" +
+                "Connected to database: <sqlcmd>\n" +
+                "Please choose an operation desired or type 'EXIT' for exiting\n" +
+                "1 - Connect to database\n" +
+                "2 - List all table names\n" +
+                "3 - Select table to work\n" +
+                "4 - Exit\n" +
+                //input - exit
+                "Terminated. Thank you for using SQLCmd. Good luck.\n", getData());
+    }
+
+    @Test
+    public void testListWithConnect() {
+        //given
+        in.addLine("1");
+        in.addLine("sqlcmd");
+        in.addLine("javauser");
+        in.addLine("test");
+        in.addLine("2");
+        in.addLine("exit");
+        //when
+        Controller.main(new String[0]);
+        //then
+        assertEquals("Welcome to SQLCmd!\n" +
+                "\n" +
+                "Please choose an operation desired or type 'EXIT' for exiting\n" +
+                "1 - Connect to database\n" +
+                "2 - List all table names\n" +
+                "3 - Select table to work\n" +
+                "4 - Exit\n" +
+                // input - 1
+                "Enter database name, login and password.\n" +
+                "Type 'exit' for exit program.\n" +
+                "\n" +
+                "Please, enter database name:\n" +
+                "Enter you login:\n" +
+                "Enter you password:\n" +
+                "Connection successful!\n" +
+                "\n" +
+                //input - 2
+                "Connected to database: <sqlcmd>\n" +
+                "Please choose an operation desired or type 'EXIT' for exiting\n" +
+                "1 - Connect to database\n" +
+                "2 - List all table names\n" +
+                "3 - Select table to work\n" +
+                "4 - Exit\n" +
+                "Available tables:\n" +
+                "[users, staff]\n" +
+                "\n" +
+                "Connected to database: <sqlcmd>\n" +
+                "Please choose an operation desired or type 'EXIT' for exiting\n" +
+                "1 - Connect to database\n" +
+                "2 - List all table names\n" +
+                "3 - Select table to work\n" +
+                "4 - Exit\n" +
+                //input - exit
+                "Terminated. Thank you for using SQLCmd. Good luck.\n", getData());
+    }
+
+    @Test
+    public void testSelectTableWithConnect() {
+        //given
+        in.addLine("1");
+        in.addLine("sqlcmd");
+        in.addLine("javauser");
+        in.addLine("test");
+        in.addLine("3");
+        in.addLine("users");
+        in.addLine("exit");
+        //when
+        Controller.main(new String[0]);
+        //then
+        assertEquals("Welcome to SQLCmd!\n" +
+                "\n" +
+                "Please choose an operation desired or type 'EXIT' for exiting\n" +
+                "1 - Connect to database\n" +
+                "2 - List all table names\n" +
+                "3 - Select table to work\n" +
+                "4 - Exit\n" +
+                // input - 1
+                "Enter database name, login and password.\n" +
+                "Type 'exit' for exit program.\n" +
+                "\n" +
+                "Please, enter database name:\n" +
+                //input - sqlcmd
+                "Enter you login:\n" +
+                //input - javauser
+                "Enter you password:\n" +
+                //input - test +
+                "Connection successful!\n" +
+                "\n" +
+                //input - 3
+                "Connected to database: <sqlcmd>\n" +
+                "Please choose an operation desired or type 'EXIT' for exiting\n" +
+                "1 - Connect to database\n" +
+                "2 - List all table names\n" +
+                "3 - Select table to work\n" +
+                "4 - Exit\n" +
+                "Enter table name. Available tables:\n" +
+                "[users, staff]\n" +
+                "\n" +
+                //input - users
+                "Connected to database: <sqlcmd>. Selected table: <users>\n" +
+                "Please choose an operation desired or type 'EXIT' for exiting\n" +
+                "1 - Print table data\n" +
+                "2 - Change table records\n" +
+                "3 - Return to previous menu\n" +
+                //input - exit
+                "Terminated. Thank you for using SQLCmd. Good luck.\n", getData());
+    }
+
+    @Test
+    public void testSelectIncorrectTableName() {
+        //given
+        in.addLine("1");
+        in.addLine("sqlcmd");
+        in.addLine("javauser");
+        in.addLine("test");
+        in.addLine("3");
+        in.addLine("usersa");
+        in.addLine("exit");
+        //when
+        Controller.main(new String[0]);
+        //then
+        assertEquals("Welcome to SQLCmd!\n" +
+                "\n" +
+                "Please choose an operation desired or type 'EXIT' for exiting\n" +
+                "1 - Connect to database\n" +
+                "2 - List all table names\n" +
+                "3 - Select table to work\n" +
+                "4 - Exit\n" +
+                // input - 1
+                "Enter database name, login and password.\n" +
+                "Type 'exit' for exit program.\n" +
+                "\n" +
+                "Please, enter database name:\n" +
+                //input - sqlcmd
+                "Enter you login:\n" +
+                //input - javauser
+                "Enter you password:\n" +
+                //input - test +
+                "Connection successful!\n" +
+                "\n" +
+                //input - 3
+                "Connected to database: <sqlcmd>\n" +
+                "Please choose an operation desired or type 'EXIT' for exiting\n" +
+                "1 - Connect to database\n" +
+                "2 - List all table names\n" +
+                "3 - Select table to work\n" +
+                "4 - Exit\n" +
+                "Enter table name. Available tables:\n" +
+                "[users, staff]\n" +
+                "\n" +
+                //input - usersa
+                "Enter correct table name. Available tables:\n" +
+                "[users, staff]\n\n" +
+                //input - exit
+                "Terminated. Thank you for using SQLCmd. Good luck.\n", getData());
+    }
+
+    @Test
+    public void testReturnWithConnect() {
+        //given
+        in.addLine("1");
+        in.addLine("sqlcmd");
+        in.addLine("javauser");
+        in.addLine("test");
+        in.addLine("3");
+        in.addLine("users");
+        in.addLine("3");
+        in.addLine("exit");
+        //when
+        Controller.main(new String[0]);
+        //then
+        assertEquals("Welcome to SQLCmd!\n" +
+                "\n" +
+                "Please choose an operation desired or type 'EXIT' for exiting\n" +
+                "1 - Connect to database\n" +
+                "2 - List all table names\n" +
+                "3 - Select table to work\n" +
+                "4 - Exit\n" +
+                // input - 1
+                "Enter database name, login and password.\n" +
+                "Type 'exit' for exit program.\n" +
+                "\n" +
+                "Please, enter database name:\n" +
+                //input - sqlcmd
+                "Enter you login:\n" +
+                //input - javauser
+                "Enter you password:\n" +
+                //input - test
+                "Connection successful!\n" +
+                "\n" +
+                "Connected to database: <sqlcmd>\n" +
+                "Please choose an operation desired or type 'EXIT' for exiting\n" +
+                "1 - Connect to database\n" +
+                "2 - List all table names\n" +
+                "3 - Select table to work\n" +
+                "4 - Exit\n" +
+                //input - 3
+                "Enter table name. Available tables:\n" +
+                "[users, staff]\n" +
+                "\n" +
+                "Connected to database: <sqlcmd>. Selected table: <users>\n" +
+                "Please choose an operation desired or type 'EXIT' for exiting\n" +
+                "1 - Print table data\n" +
+                "2 - Change table records\n" +
+                "3 - Return to previous menu\n" +
+                //input - 3
+                "Connected to database: <sqlcmd>\n" +
+                "Please choose an operation desired or type 'EXIT' for exiting\n" +
+                "1 - Connect to database\n" +
+                "2 - List all table names\n" +
+                "3 - Select table to work\n" +
+                "4 - Exit\n" +
+                //input - exit
                 "Terminated. Thank you for using SQLCmd. Good luck.\n", getData());
     }
 
