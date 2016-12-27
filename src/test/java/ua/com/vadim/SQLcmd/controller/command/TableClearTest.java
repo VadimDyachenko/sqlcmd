3fa720403646d62d4514cfd7929c9a63f6b90bce
Vadim Dyachenko <vadim.dyachenko@gmail.com>
2016-12-27 09:14:26 +0200

    After CodeReview: Упростил юнит-тест команды TableClear

--- src/test/java/ua/com/vadim/SQLcmd/controller/command/TableClearTest.java
+++ src/test/java/ua/com/vadim/SQLcmd/controller/command/TableClearTest.java
@@ -7,64 +7,64 @@ import ua.com.vadim.SQLcmd.controller.PropertiesLoader;
 import ua.com.vadim.SQLcmd.controller.RunParameters;
 import ua.com.vadim.SQLcmd.exception.ExitException;
 import ua.com.vadim.SQLcmd.model.DatabaseManager;
-import ua.com.vadim.SQLcmd.view.UTF8Control;
 import ua.com.vadim.SQLcmd.view.View;
 
 import java.sql.SQLException;
-import java.util.ResourceBundle;
+import java.util.Locale;
 
 import static org.mockito.Mockito.*;
 
 public class TableClearTest extends AbstractCommandTest{
-    private static RunParameters runParameters;
-    private static ResourceBundle resources;
+    private static RunParameters parameters;
+    private View view;
     private DatabaseManager manager;
     private Command command;
     private final String testTableName = "tableA";
-    private String questionFormatted;
 
     @BeforeClass
     public static void beforeAllTestSetUp() throws ExitException {
-        runParameters = new PropertiesLoader().getParameters();
-        resources = ResourceBundle.getBundle("TableClear", new UTF8Control());
+        Locale.setDefault(Locale.ENGLISH);
+        parameters = new PropertiesLoader().getParameters();
     }
 
     @Before
     public void setUp() throws Exception {
         manager = mock(DatabaseManager.class);
         view = mock(View.class);
-        command = new TableClear(runParameters, manager, view);
-
-        String question = resources.getString("table.clear.question");
-        runParameters.setTableName(testTableName);
-        questionFormatted = String.format(question, testTableName);
+        command = new TableClear(parameters, manager, view);
+        parameters.setTableName(testTableName);
     }
 
     @Test
     public void testTableClearWithYesAnswer() throws Exception {
         //given
-        String successful = resources.getString("table.clear.successful");
-        String expectedMessage = String.format("[%s, %s]", questionFormatted, successful);
-        //when
         when(manager.isConnected()).thenReturn(true);
         when(view.readLine()).thenReturn("y");
+
+        //when
         command.execute();
+
         //then
-        shouldPrint(expectedMessage);
+        shouldPrint("[Do you really want to clear table <tableA>? <y/n>, Table clear successful.]");
     }
 
     @Test
     public void testTableClearWithSQLException() throws Exception {
         //given
-        String exceptionMessage = "Some SQLException";
-        String fail = resources.getString("table.clear.failed");
-        String expectedMessage = String.format("[%s, %s%s]", questionFormatted, fail, exceptionMessage);
-        //then
-        doThrow(new SQLException(exceptionMessage)).when(manager).clearTable(testTableName);
+        doThrow(new SQLException("Some SQLException")).when(manager).clearTable(testTableName);
         when(view.readLine()).thenReturn("y");
-        command.execute();
+
         //then
-        shouldPrint(expectedMessage);
+        command.execute();
+
+        //then
+        shouldPrint("[Do you really want to clear table <tableA>? <y/n>," +
+                " Table is not cleared because:Some SQLException]");
         //when
+    }
+
+    @Override
+    View getView() {
+        return view;
     }
 }
