1434ef78a153565e02c09af6d2d54674c504a640
Vadim Dyachenko <vadim.dyachenko@gmail.com>
2016-12-27 09:27:09 +0200

    After CodeReview: Упростил юнит-тест TablePrintData

--- src/test/java/ua/com/vadim/SQLcmd/controller/command/TablePrintDataTest.java
+++ src/test/java/ua/com/vadim/SQLcmd/controller/command/TablePrintDataTest.java
@@ -3,39 +3,37 @@ package ua.com.vadim.SQLcmd.controller.command;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
+
 import ua.com.vadim.SQLcmd.controller.PropertiesLoader;
 import ua.com.vadim.SQLcmd.controller.RunParameters;
 import ua.com.vadim.SQLcmd.exception.ExitException;
 import ua.com.vadim.SQLcmd.model.DataSet;
 import ua.com.vadim.SQLcmd.model.DataSetImpl;
 import ua.com.vadim.SQLcmd.model.DatabaseManager;
-import ua.com.vadim.SQLcmd.view.UTF8Control;
 import ua.com.vadim.SQLcmd.view.View;
 
 import java.sql.SQLException;
-import java.util.ResourceBundle;
+import java.util.Locale;
 
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class TablePrintDataTest extends AbstractCommandTest{
-    private static RunParameters runParameters;
-    private static ResourceBundle resources;
+    private static RunParameters parameters;
+    private View view;
     private DatabaseManager manager;
     private Command command;
 
     @BeforeClass
     public static void beforeAllTestSetUp() throws ExitException {
-        runParameters = new PropertiesLoader().getParameters();
-        resources = ResourceBundle.getBundle("TablePrintData", new UTF8Control());
+        Locale.setDefault(Locale.ENGLISH);
+        parameters = new PropertiesLoader().getParameters();
     }
 
     @Before
     public void setUp() throws Exception {
         manager = mock(DatabaseManager.class);
         view = mock(View.class);
-        command = new TablePrintData(runParameters, manager, view);
+        command = new TablePrintData(parameters, manager, view);
     }
 
     @Test
@@ -43,14 +41,14 @@ public class TablePrintDataTest extends AbstractCommandTest{
         //given
         DataSet[] tableData = new DataSetImpl[0];
         String tableName = "tableA";
-        runParameters.setTableName(tableName);
-        String messageFormatted = String.format(resources.getString("table.print.data.empty"), tableName);
-        String expectedMessage = String.format("[%s]", messageFormatted);
-        //when
+        parameters.setTableName(tableName);
         when(manager.getTableData(tableName)).thenReturn(tableData);
+
+        //when
         command.execute();
+
         //then
-        shouldPrint(expectedMessage);
+        shouldPrint("[Table <tableA> is empty.]");
     }
 
     @Test
@@ -62,31 +60,34 @@ public class TablePrintDataTest extends AbstractCommandTest{
         dataSet.put("name", "TestName");
         dataSet.put("password", "qwerty");
         tableData[0] = dataSet;
+
         String tableName = "tableA";
-        runParameters.setTableName(tableName);
-        String expectedMessage = "[|id|name    |password|, " +
-                                  "+--+--------+--------+, " +
-                                  "|1 |TestName|qwerty  |]";
-        //when
+        parameters.setTableName(tableName);
         when(manager.getTableData(tableName)).thenReturn(tableData);
+
+        //when
         command.execute();
+
         //then
-        shouldPrint(expectedMessage);
+        shouldPrint("[|id|name    |password|, +--+--------+--------+, |1 |TestName|qwerty  |]");
     }
 
     @Test
     public void testTablePrintDataWithSQLException() throws SQLException {
         //given
         String tableName = "tableA";
-        runParameters.setTableName(tableName);
-        String exceptionMessage = "Some SQLException";
-        String expectedMessage = String.format("[%s%s]",
-                resources.getString("table.print.data.failed"),
-                exceptionMessage);
+        parameters.setTableName(tableName);
+
+        when(manager.getTableData(tableName)).thenThrow(new SQLException("Some SQLException"));
         //when
-        when(manager.getTableData(tableName)).thenThrow(new SQLException(exceptionMessage));
+
         command.execute();
         //then
-        shouldPrint(expectedMessage);
+        shouldPrint("[Failure because:Some SQLException]");
+    }
+
+    @Override
+    View getView() {
+        return view;
     }
 }
