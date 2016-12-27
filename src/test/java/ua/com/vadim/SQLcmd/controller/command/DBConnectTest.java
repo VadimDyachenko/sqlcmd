180bf5abbe81f510c179b58092b7d62bbfb7b715
Vadim Dyachenko <vadim.dyachenko@gmail.com>
2016-12-27 08:42:53 +0200

    After CodeReview: Упростил юнит-тест команды DBConnect

--- src/test/java/ua/com/vadim/SQLcmd/controller/command/DBConnectTest.java
+++ src/test/java/ua/com/vadim/SQLcmd/controller/command/DBConnectTest.java
@@ -11,88 +11,97 @@ import ua.com.vadim.SQLcmd.model.DatabaseManager;
 import ua.com.vadim.SQLcmd.model.PostgresDBManager;
 import ua.com.vadim.SQLcmd.view.Console;
 import ua.com.vadim.SQLcmd.view.UTF8Control;
+import ua.com.vadim.SQLcmd.view.View;
 
 import java.sql.SQLException;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.*;
 
 public class DBConnectTest extends AbstractCommandTest{
-    private static RunParameters runParameters;
+    private static RunParameters parameters;
     private static ResourceBundle resources;
+    private View view;
     private DatabaseManager manager;
     private Command command;
     private String normalRunMessages;
 
     @BeforeClass
     public static void beforeAllTestSetUp() throws ExitException {
-        runParameters = new PropertiesLoader().getParameters();
-        resources = ResourceBundle.getBundle("DBConnect", new UTF8Control());
+        Locale.setDefault(Locale.ENGLISH);
+        parameters = new PropertiesLoader().getParameters();
     }
 
     @Before
     public void setup() {
         manager = mock(PostgresDBManager.class);
         view = mock(Console.class);
-        command = new DBConnect(runParameters, manager, view);
-        String m1 = resources.getString("dbconnect.enter.connection.parameters");
-        String m2 = resources.getString("dbconnect.enter.database.name");
-        String m3 = resources.getString("dbconnect.enter.login");
-        String m4 = resources.getString("dbconnect.enter.password");
-        normalRunMessages = String.format("%s, %s, %s, %s", m1, m2, m3, m4);
+        command = new DBConnect(parameters, manager, view);
     }
 
     @Test
     public void testConnectDefaultParameter() {
         //given
-        String expectedMessage = String.format("[%s]", resources.getString("dbconnect.successful"));
-        //when
         when(manager.isConnected()).thenReturn(false);
+
+        //when
         command.execute();
+
         //then
-        shouldPrint(expectedMessage);
+        shouldPrint("[Connection successful!]");
     }
 
-    @Test (expected = ExitException.class)
+    @Test
     public void testConnectDefaultParameterWithSQLException() throws SQLException {
         //given
-        String exceptionMessage = "Some SQLException";
-        String failedMessage = resources.getString("dbconnect.failed");
-        String tryAgainMessage = resources.getString("dbconnect.default.failed");
-        String expectedMessage = String.format("[%s %s, %s]",failedMessage, exceptionMessage,
-                tryAgainMessage);
-        //when
         when(manager.isConnected()).thenReturn(false);
-        doThrow(new SQLException(exceptionMessage)).when(manager).connect(anyString(), anyString(), anyString());
-        command.execute();
+        doThrow(new SQLException("Some SQLException")).when(manager).connect(anyString(), anyString(), anyString());
+
+        //when
+        try {
+            command.execute();
+            fail("expected excetion");
+        } catch (ExitException e) {
+            assertEquals(null, e.getMessage());
+        }
+
         //then
-        shouldPrint(expectedMessage);
+        shouldPrint("[Connection failed: Some SQLException," +
+                " Please check connection parameter in sqlcmd.properties file and try again.]");
     }
 
     @Test
     public void testConnectNewParameter() throws SQLException {
         //given
-        String expectedMessage = String.format("[%s]", normalRunMessages);
-        //when
         when(manager.isConnected()).thenReturn(true);
+
+        //when
         command.execute();
+
         //then
-        shouldPrint(expectedMessage);
+        shouldPrint("[Please, enter new connection parameters or type <exit> to exit programm.," +
+                " Enter database name:, Enter you login:, Enter you password:]");
     }
 
     @Test
     public void testConnectNewParameterWithSQLException() throws SQLException {
         //given
-        String exceptionMessage = "Some SQLException";
-        String failedMessage = resources.getString("dbconnect.failed");
-        String tryAgainMessage = resources.getString("dbconnect.try.again");
-        String expectedMessage = String.format("[%s, %s %s, %s]", normalRunMessages, failedMessage,
-                exceptionMessage, tryAgainMessage);
+        doThrow(new SQLException("Some SQLException")).when(manager).connect(anyString(), anyString(), anyString());
+
         //when
-        doThrow(new SQLException(exceptionMessage)).when(manager).connect(anyString(), anyString(), anyString());
         when(manager.isConnected()).thenReturn(true);
         command.execute();
         //then
-        shouldPrint(expectedMessage);
+        shouldPrint("[Please, enter new connection parameters or type <exit> to exit programm., " +
+                "Enter database name:, Enter you login:, Enter you password:, " +
+                "Connection failed: Some SQLException, Try again or type <exit> to exit programm.]");
+    }
+
+    @Override
+    View getView() {
+        return view;
     }
 }
