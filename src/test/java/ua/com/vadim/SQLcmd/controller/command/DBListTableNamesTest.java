28c1734333885db39097e7fea3e8487e57e3b772
Vadim Dyachenko <vadim.dyachenko@gmail.com>
2016-12-27 08:50:44 +0200

    After CodeReview: Упростил юнит-тест команды DBListTableNames

--- src/test/java/ua/com/vadim/SQLcmd/controller/command/DBListTableNamesTest.java
+++ src/test/java/ua/com/vadim/SQLcmd/controller/command/DBListTableNamesTest.java
@@ -7,44 +7,44 @@ import ua.com.vadim.SQLcmd.controller.PropertiesLoader;
 import ua.com.vadim.SQLcmd.controller.RunParameters;
 import ua.com.vadim.SQLcmd.exception.ExitException;
 import ua.com.vadim.SQLcmd.model.DatabaseManager;
-import ua.com.vadim.SQLcmd.view.UTF8Control;
 import ua.com.vadim.SQLcmd.view.View;
 
 import java.sql.SQLException;
 import java.util.LinkedHashSet;
-import java.util.ResourceBundle;
+import java.util.Locale;
 import java.util.Set;
 
 import static org.mockito.Mockito.*;
 
 public class DBListTableNamesTest extends AbstractCommandTest{
-    private static RunParameters runParameters;
-    private static ResourceBundle resources;
+    private static RunParameters parameters;
+    private View view;
     private DatabaseManager manager;
     private Command command;
 
     @BeforeClass
     public static void beforeAllTestSetUp() throws ExitException {
-        runParameters = new PropertiesLoader().getParameters();
-        resources = ResourceBundle.getBundle("DBListTableNames", new UTF8Control());
+        Locale.setDefault(Locale.ENGLISH);
+        parameters = new PropertiesLoader().getParameters();
     }
 
     @Before
     public void setUp() {
         manager = mock(DatabaseManager.class);
         view = mock(View.class);
-        command = new DBListTableNames(runParameters, manager, view);
+        command = new DBListTableNames(parameters, manager, view);
     }
 
     @Test
     public void testListTableNamesWithoutConnectionToDatabase() throws Exception {
         //given
-        String expectedMessage = String.format("[%s]", resources.getString("dblist.no.connection"));
-        //when
         when(manager.isConnected()).thenReturn(false);
+
+        //when
         command.execute();
+
         //then
-        shouldPrint(expectedMessage);
+        shouldPrint("[No one connection to database. Select \"Connect to database\" first.\n]");
     }
 
     @Test
@@ -54,46 +54,44 @@ public class DBListTableNamesTest extends AbstractCommandTest{
         tableNames.add("id");
         tableNames.add("names");
         tableNames.add("password");
-        String expectedResult = "[id, names, password]";
-        String expectedMessage = String.format("[%s, %s]",
-                resources.getString("dblist.available.tables"),
-                expectedResult);
-        //when
         when(manager.isConnected()).thenReturn(true);
         when(manager.getAllTableNames()).thenReturn(tableNames);
+
+        //when
         command.execute();
 
         //then
-        shouldPrint(expectedMessage);
+        shouldPrint("[Available tables:, [id, names, password]]");
     }
 
     @Test
     public void testListTableNamesWithEmptyDatabase() throws Exception {
         //given
         Set<String> tableNames = new LinkedHashSet<>();
-        String expectedMessage = String.format("[%s]", resources.getString("dblist.no.tables"));
-        //when
         when(manager.isConnected()).thenReturn(true);
         when(manager.getAllTableNames()).thenReturn(tableNames);
+
+        //when
         command.execute();
         //then
-        shouldPrint(expectedMessage);
+        shouldPrint("[There are no any tables in the database.]");
     }
 
     @Test
     public void testListTableNamesWithSQLException() throws Exception {
         //given
-        String exceptionMessage = "Some SQLMessage";
-        String expectedMessage = String.format("[%s%s, %s]",
-                resources.getString("dblist.failure"),
-                exceptionMessage,
-                resources.getString("dblist.no.tables"));
-        //when
         when(manager.isConnected()).thenReturn(true);
-        doThrow(new SQLException(exceptionMessage)).when(manager).getAllTableNames();
+        doThrow(new SQLException("Some SQLException")).when(manager).getAllTableNames();
+
+        //when
         command.execute();
 
         //then
-        shouldPrint(expectedMessage);
+        shouldPrint("[Failure, because:Some SQLException, There are no any tables in the database.]");
+    }
+
+    @Override
+    View getView() {
+        return view;
     }
 }
